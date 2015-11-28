a626fd661ff4593b514abd19822e8753ca222957
Pablo Picado Morales <pablopicado02@gmail.com>
2015-11-28 16:22:06 -0600

    Script para saludar a nadie

--- Scripts/LAB03/saluda.sh
+++ Scripts/LAB03/saluda.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
 if [ $# == 0 ]
 then 
  echo "Debe usar un parámetro posicional"
 else
-   if [$1=="Pablo"]
+   if [ $1== "Pablo"]
    then  
     echo "Hola Pablo"
+   elif [ $1 == "nadie"]
+   then
+    echo "No puedo saludar a nadie"
    else
      echo "Quién eres????"
      read nombre
