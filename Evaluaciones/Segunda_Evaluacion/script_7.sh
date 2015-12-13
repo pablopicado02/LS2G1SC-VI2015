37e8548d579ce2010a234fd5e1e23de0b59c11f0
Pablo <you@example.com>
2015-12-13 00:54:08 -0600

    Se utiliza pgrep para determinar la existencia del proceso y se crea el reporte

---
+++ Evaluaciones/Segunda_Evaluacion/script_7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Cree un script que permita determinar si un determinado proceso está activo o no, deberá de generar un registro cada vez que se ejecute indicando, la fecha, el nombre del proceso y el estado (ACTIVO o INACTIVO).
+echo "Dijite el nombre del proceso"
+read proceso
+pgrep $proceso &> /dev/null
+
+  if [ $? = 1 ]; then
+   estado="INACTIVO"
+  else
+   estado="ACTIVO"
+  fi
+#Se crea el reporte
+echo "
+       Reporte
+      ---------
+
+Fecha:$(date +"%d|%m|%y")     
+Proceso:$proceso
+Estado:$estado
+---------------------------
+"> "Reporte-$proceso".txt
+echo "Reporte creado correctamente"
